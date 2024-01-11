ee614773950226a1d5c9fdc3aa289464067244f3
booennm <booennm@gmail.com>
2024-01-11 19:04:45 +0200

    about page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const About = () => {
   return (
-    <h1>About</h1>
+    <div className="App-content">
+      <Container>
+        <Row>
+          <Col>
+            <h1>About</h1>
+            <p>CalendarUs is a collaborative calendar for planning events that fit into everyones schedule</p>
+            <h1>How to use CalendarUs</h1>
+            <p>
+              The home page showcases all of your calendars, including the ones shared to you by other users.
+              This is also where you can create new calendars. Add users to created calendars by their account email.
+              The calendar shows unavailability from all added users as red backgrounds. Set your unavailability in
+              your profile to let others know when you're busy.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
